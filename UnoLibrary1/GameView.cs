8a5471ceff434de2aeed2f46556221d60c74e3c5
Martin Zikmund <martin@zikmund.dev>
2023-11-04 09:37:30 +0100

    Issure repro

---
+++ UnoLibrary1/GameView.cs
@@ -0,0 +1,13 @@
+﻿using Alchemy.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AndroidApp2
+{
+    public partial class GameView : ViewBase<GameViewModel>
+    {
+    }
+}
---
+++ UnoLibrary1/ViewBase.cs
@@ -0,0 +1,12 @@
+﻿using AndroidApp2;
+using Microsoft.UI.Xaml.Controls;
+
+namespace Alchemy.Views;
+
+public abstract partial class ViewBase<TViewModel> : Page
+    where TViewModel : PageViewModel
+{
+    protected ViewBase()
+    {
+    }
+}
